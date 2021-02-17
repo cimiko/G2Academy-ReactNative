288043a2bc38bc7890b9cf2f28f5554fa1e0d680
CiMiKo <private.hartono@mail.com>
2021-02-17 22:53:01 +0700

    .

---
+++ LiveCode2/src/store/index.js
@@ -0,0 +1,13 @@
+import React, { createContext, useReducer } from 'react'
+
+
+const CartContext = createContext();
+
+function CartProvider({children}) {
+    return (
+        // const [cartState, setCartState] = useReducer(cartReducer, []);
+
+    )
+}
+
+export default CartProvider
