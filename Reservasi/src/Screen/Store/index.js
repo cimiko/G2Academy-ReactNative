7e8a1c82410ddf88086a268117a81a5e7abebbda
CiMiKo <private.hartono@mail.com>
2021-03-09 16:22:53 +0700

    Menambahkan Screen Store

---
+++ Reservasi/src/Screen/Store/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { SafeAreaView, ScrollView, View } from 'react-native'
+
+function Store() {
+    return (
+        <View>
+            <SafeAreaView>
+                <ScrollView>
+                    
+                </ScrollView>
+            </SafeAreaView>
+        </View>
+    )
+}
+
+export default Store
+
